04d0fa29c4b4b9efb4ed939c6acb55309023c2d2
Willaraque <152440592+Willaraque@users.noreply.github.com>
2026-08-12 15:05:50 +0200

    Añadiendo archivos

--- client/src/api/Tokens.js
+++ client/src/api/Tokens.js
@@ -1,5 +1,7 @@
 import { apiClient } from "./client";
 
+
+
 export const AccessToken = (username, password) => apiClient.post("/token", new URLSearchParams({ username, password }), { headers: { "Content-Type": "application/x-www-form-urlencoded" } });
 export const VerifyToken = (token) => apiClient.post("/verify-token", { token });
 export const deleteToken = (_id) => apiClient.delete("/delete-token", { data: { _id } });
--- client/src/api/Users.js
+++ client/src/api/Users.js
@@ -1,5 +1,7 @@
 import { apiClient } from "./client";
 
+
+
 const endpoint = "/api/users";
 export const createUser = (user) => apiClient.post(`${endpoint}/create`, user);
 export const fetchUser = (id) => apiClient.get(`${endpoint}/${id}`);
