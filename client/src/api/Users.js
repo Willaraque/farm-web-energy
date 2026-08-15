241f284ec2d05db90665a91b1a2b1720cc88d4a9
Willaraque <152440592+Willaraque@users.noreply.github.com>
2026-08-15 12:48:29 +0200

    drop antiguo Users.js

--- client/src/api/Users.js
+++
@@ -1,9 +0,0 @@
-import { apiClient } from "./client";
-
-
-const endpoint = "/api/users";
-export const createUser = (user) => apiClient.post(`${endpoint}/create`, user);
-export const fetchUser = (id) => apiClient.get(`${endpoint}/${id}`);
-export const updateUser = (id, user) =>
-  apiClient.put(`${endpoint}/update/${id}`, user);
-export const deleteUser = (id) => apiClient.delete(`${endpoint}/delete/${id}`);
